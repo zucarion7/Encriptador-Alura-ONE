075b31ae6b262b7d30bb1e3e367640d5d1aae7a6
zucarion7 <zucarion@gmail.com>
2022-12-27 13:23:41 -0500

    Primer commit Inicio de proyecto Encriptador

---
+++ js/index.js
@@ -0,0 +1,66 @@
+function validar(frase,encript){
+    if(validarEmpty(frase)){
+        alert("No se encontró ningun mensaje");
+    }else{
+        for(let i=0;i<frase.length;i++){
+            if(!((frase.charCodeAt(i)>96 && frase.charCodeAt(i)<123) || (frase.charCodeAt(i)>47 && frase.charCodeAt(i)<58))){
+                alert(`Se ingreso letra mayuscula, tilde o caracter especial. \n\nNo se puede ${encript}`)
+                return false;
+            }
+        };    
+        return true;
+    }
+}
+
+function validarEmpty(frase){
+    let vacio=frase=="" ? true:false;
+    return vacio;
+}
+
+function encriptar(frase){
+    frase=frase.replaceAll('e','enter');    
+    frase=frase.replaceAll('i','imes');
+    frase=frase.replaceAll('a','ai');        
+    frase=frase.replaceAll('o','ober');
+    frase=frase.replaceAll('u','ufat');    
+    return(frase);
+}
+
+function desEncriptar(frase){
+    frase=frase.replaceAll('enter','e');
+    frase=frase.replaceAll('imes','i');
+    frase=frase.replaceAll('ai','a');        
+    frase=frase.replaceAll('ober','o');
+    frase=frase.replaceAll('ufat','u');
+    return frase;
+}
+
+let dato=document.querySelector("[name=texto]");
+
+const encriptador=document.querySelector("[name=encriptar]");
+encriptador.addEventListener("click",()=>{
+        if(validar(dato.value,"encriptar")==true) alert(encriptar(dato.value))
+});
+
+const desEncriptador=document.querySelector("[name=desEncriptar]");
+desEncriptador.addEventListener("click",()=>{
+        if(validar(dato.value,"des-encriptar")==true) alert(desEncriptar(dato.value))
+});
+
+
+
+
+
+
+
+// const encriptador=document.querySelector("[name=encriptar]");
+// encriptador.addEventListener("click",()=>alert(encriptar(dato.value))); //encriptar sin importar validacion
+
+
+
+// let body=document.querySelector("body");
+// body.addEventListener("click",(event)=>{
+//     if(event.target.name=="encriptar"){alert(encriptar(dato.value))}
+//     if(event.target.name=="desEncriptar"){alert(desEncriptar(dato.value))}
+// })
+
