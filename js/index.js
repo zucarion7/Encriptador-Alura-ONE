4efa53116e4f0bd7952ffc1a26ee47e4dcb7803a
zucarion7 <zucarion@gmail.com>
2022-12-30 02:34:23 -0500

    dando funcion al boton copiar y mas estilos

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 function validar(frase,encript){
     if(validarEmpty(frase)){            
         for(let i=0;i<frase.length;i++){
-            if(!((frase.charCodeAt(i)>96 && frase.charCodeAt(i)<123) || (frase.charCodeAt(i)>47 && frase.charCodeAt(i)<58))){
+            if(!((frase.charCodeAt(i)==32) || (frase.charCodeAt(i)==241) || (frase.charCodeAt(i)>96 && frase.charCodeAt(i)<123) || (frase.charCodeAt(i)>47 && frase.charCodeAt(i)<58))){
                 alert(`Se ingreso letra mayuscula, tilde o caracter especial. \n\nNo se puede ${encript}`)
                 return false;
             }
@@ -35,25 +35,42 @@ function desEncriptar(frase){
     return frase;
 }
 
-let dato=document.querySelector("[name=texto]");
-const copiar=document.querySelector("[name=copiar]");
-const limpiar_texto=document.querySelector("[name=limpiar_texto");
-const limpiar_mensaje=document.querySelector("[name=limpiar_mensaje");
+function copiarPortaPapeles(elemento){
+    let textoACopiar=elemento.value;
+    navigator.clipboard.writeText(textoACopiar);
+}
+
+function pegarPortaPapeles(elemento){   
+    elemento.select(); 
+    navigator.clipboard.readText()
+        .then((value)=>elemento.value=value);
+    // alert(elemento.value);
+}
+
+let texto=document.querySelector("[name=texto]");
 const encriptador=document.querySelector("[name=encriptar]");
 const desEncriptador=document.querySelector("[name=desEncriptar]");
-let output=document.querySelector("output");
+const limpiar_texto=document.querySelector("[name=limpiar_texto");
+let mensaje=document.querySelector("[name=mensaje]");
+const copiar=document.querySelector("[name=copiar]");
+const limpiar_mensaje=document.querySelector("[name=limpiar_mensaje");
 
 encriptador.addEventListener("click",()=>{
-        if(validar(dato.value,"encriptar")==true) alert(encriptar(dato.value))
+        if(validar(texto.value,"encriptar")==true) mensaje.value=encriptar(texto.value);
 });
 
 desEncriptador.addEventListener("click",()=>{
-        if(validar(dato.value,"des-encriptar")==true) alert(desEncriptar(dato.value))
+        if(validar(texto.value,"des-encriptar")==true) mensaje.value=desEncriptar(texto.value);
 });
 
-copiar.addEventListener("click",()=>alert("test"));
-// limpiar_texto.addEventListener("click",()=>alert("hola"));
-limpiar_mensaje.addEventListener("click",()=>alert("test3"));
+limpiar_texto.addEventListener("click",()=>texto.value="");
+
+copiar.addEventListener("click",()=>{
+    copiarPortaPapeles(mensaje);
+    pegarPortaPapeles(texto);    
+});
+
+limpiar_mensaje.addEventListener("click",()=>mensaje.value="");
 
 
 
